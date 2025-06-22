f0e98b4783df04de1e5e1cef3e53b2597440215a
unknown <eran.hasarali.2001@gmail.com>
2025-06-22 22:53:19 +0530

    add .env config for db

--- server/db/db.js
+++ server/db/db.js
@@ -1,14 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mysql from "mysql2/promise";
 
 // Create a MySQL connection pool
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "my_db",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 export default pool;
